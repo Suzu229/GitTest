40f0c208a48201dafdb3ef59a9a480d5f61a0fbd
Suzu229 <suzurimi.ken@gmail.com>
2022-12-11 19:46:58 +0900

    プロジェクト ファイルを追加します。

---
+++ GitTest/Form1.cs
@@ -0,0 +1,13 @@
+namespace GitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "So, my desire is... an immortal body and infinite potential for growth!!";
+
+        }
+    }
+}
